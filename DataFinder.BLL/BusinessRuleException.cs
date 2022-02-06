cfa7ae0c9973727084e164bccf454d3d42a2b482
Khalmatov <large000@yandex.ru>
2022-02-06 15:30:55 +0300

    Использвал разные проекты(сборки) для разделение приложения на слои (Слой клиента,Слой логики приложения,Слой данных)

---
+++ DataFinder.BLL/BusinessRuleException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataFinder.BLL
+{
+    /// <summary>
+    /// пользовательское исключение
+    /// </summary>
+    public class BusinessRuleException : Exception
+    {
+        public BusinessRuleException(string message) : base(message)
+        {
+
+        }
+    }
+}
